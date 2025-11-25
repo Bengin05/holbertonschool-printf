3deab8bb35aa1ead34a60d73e35c55054ee7fefb
Safwann8 <safoulefou83@gmail.com>
2025-11-25 15:08:39 +0000

    creation of the _printf.c file / handle-string coding

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+int handle_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (!str)
+		str = "(null)";
+
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		count++;
+	}
+
+	return (count);
+}
