ace37b4cbb65979fff319ec69e3fd4e5b5648efb
Safwann8 <safoulefou83@gmail.com>
2025-11-27 09:55:22 +0000

    betty modification for the handle_decimal.c file

--- handle_decimal.c
+++ handle_decimal.c
@@ -12,11 +12,10 @@ int handle_decimal(va_list args)
 {
 	int n = va_arg(args, int);
 	int count = 0;
-	long num = n;          /* utiliser long pour gérer INT_MIN */
-	char buffer[12];       /* assez pour -2147483648 */
+	long num = n;
+	char buffer[12];
 	int i = 0;
 
-	/* gérer le signe négatif */
 	if (num < 0)
 	{
 		write(1, "-", 1);
@@ -24,21 +23,18 @@ int handle_decimal(va_list args)
 		num = -num;
 	}
 
-	/* gérer le cas où num = 0 */
 	if (num == 0)
 	{
 		write(1, "0", 1);
 		return (count + 1);
 	}
 
-	/* convertir num en chaîne */
 	while (num > 0)
 	{
 		buffer[i++] = (num % 10) + '0';
 		num /= 10;
 	}
 
-	/* afficher dans le bon ordre */
 	while (i--)
 	{
 		write(1, &buffer[i], 1);
